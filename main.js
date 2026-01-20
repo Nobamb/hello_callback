b7ce6be2cd832b63b873e0e058ef58c3b9333aa5
nobamb <jsh12345644@gmail.com>
2026-01-20 22:49:50 +0900

    callbackTest 동작문 코드 추가

--- main.js
+++ main.js
@@ -44,9 +44,34 @@ const callbackTest = (first, second, third, fourth, value1, value2) => {
     // 만약에 first, second, third, fourth 모두 function이면
     if(typeof(first) === "function" && typeof(second) === "function"&& typeof(third) === "function"&& typeof(fourth) === "function"){
         // 테스트
-        console.log("모두 함수입니다")
+        // console.log("모두 함수입니다")
+
+        // first 먼저 실행
+        const first = first(value1, value2)
+
+        // second 다음에 실행
+
+        const second = second(value1, value2)
+
+        // third 다음에 실행
+
+        const third = third(value1, value2)
+
+
+        // fourth 마지막 실행
+        const fourth = fourth(value1, value2)
+
+
+        // 출력
+        console.log(`첫번째 함수 값 ${first}`)
+        console.log(`두번째 함수 값 ${second}`)
+        console.log(`세번째 함수 값 ${third}`)
+        console.log(`네번째 함수 값 ${fourth}`)
+
 
     }
+
+
 
 }
 
