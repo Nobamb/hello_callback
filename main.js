3e5be5751a12e3dcb8c89e887af6a0955e828a72
nobamb <jsh12345644@gmail.com>
2026-01-20 22:42:03 +0900

    callbackTest 함수 주석 작성

--- main.js
+++ main.js
@@ -25,3 +25,16 @@ const divide = (a,b)=>{
     return a / b
 
 }
+
+
+// 콜백함수를 만들어서 테스트를 해본다.
+// 함수명은 callbackTest
+// 파라미터는 각각 first, second, third, fourth, value1, value2
+// 파라미터는 총 6개
+// callbackTest 함수에 4가지의 함수 add, minus, multiple, divide를 넣는다.
+// first, second, third, fourth에
+// 그리고 2개의 변수를 넣는다. 
+// value1, value2
+// 만약에 first, second, third, fourth 모두 함수일 때 그대로 실행
+// 하나라도 함수가 아니면 오류 발생
+
