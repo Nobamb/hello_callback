0d734db5d2ec8c8c8516419e3fa55eca0fdd3e18
nobamb <jsh12345644@gmail.com>
2026-01-21 17:26:45 +0900

    minus함수 변경(콜백함수 실행하도록)

--- func/minus.js
+++ func/minus.js
@@ -1,8 +1,12 @@
 // minus 함수 생성
-const minus = (value1, value2)=>{
+const minus = (value1, value2, func)=>{
 
-    return value1 - value2
+    const result = value1 - value2
     
+    // callback함수 실행
+    func(result)
+
+
 }
 // export
 export default minus
