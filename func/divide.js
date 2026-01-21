37f0777806c563e56e338196a0d5f3617ec84e7f
nobamb <jsh12345644@gmail.com>
2026-01-21 17:27:31 +0900

    divide함수 변경(콜백함수 실행하도록)

--- func/divide.js
+++ func/divide.js
@@ -1,8 +1,11 @@
 // divide 함수 생성
-const divide = (value1, value2) => {
+const divide = (value1, value2, func) => {
 
-    // 값 출력
-    return value1 / value2
+    // 값 지정
+    const result = value1 / value2
+
+    // 콜백함수 실행
+    func(result)
 
 }
 
