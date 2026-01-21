8f0a4cee5f99db48e0343396650e6bb01c830cb9
nobamb <jsh12345644@gmail.com>
2026-01-21 17:10:41 +0900

    진정한 콜백 지옥 테스트

---
+++ asyncCallback2.js
@@ -0,0 +1,29 @@
+// 진정한 콜백 지옥
+// plus minus multiple divide import
+
+import plus from "./func/plus.js";
+import minus from "./func/minus.js";
+import multiple from "./func/multiple.js";
+import divide from "./func/divide.js";
+
+// 콜백 지옥 테스트
+// 값 더하기
+const result = plus(2,3,(result1)=>{
+
+    // 값 빼기
+    minus(result1,2,(result2)=>{
+
+        // 값 곱하기
+        multiple(result2, 6, (result3)=>{
+
+            // 값 나누기
+            divide(result3, 3)
+
+        })
+
+    })
+
+})
+
+// 출력
+console.log(result)
