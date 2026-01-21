65346de63db4b4c997881f817ad2885606441373
nobamb <jsh12345644@gmail.com>
2026-01-21 17:27:17 +0900

    multiple함수 변경(콜백함수 실행하도록)

--- func/multiple.js
+++ func/multiple.js
@@ -1,8 +1,13 @@
 // multiple 함수 생성
-const multiple = (value1, value2) =>{
+const multiple = (value1, value2, func) =>{
 
-    // 곱한 값 return
-    return value1 * value2
+    // 곱한 값
+    const result = value1 * value2
+
+    // 콜백함수 실행
+    func(result)
+
+
 
 }
 // export
